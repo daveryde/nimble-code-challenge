01c914280a9873a7f8e8229964ff1a6e8bb9f594
David Cordova <d.mcordova87@gmail.com>
2021-10-15 22:54:18 -0700

    Updates

---
+++ precache-manifest.bf0085e37dd775e8418ce362f856673f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7cf188f54a7d44ded4faf6b15b68c97",
+    "url": "/nimble-code-challenge/index.html"
+  },
+  {
+    "revision": "346151580287cbfded1d",
+    "url": "/nimble-code-challenge/static/css/main.a9a754ca.chunk.css"
+  },
+  {
+    "revision": "e354abf60dfd1c0536da",
+    "url": "/nimble-code-challenge/static/js/2.937558a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nimble-code-challenge/static/js/2.937558a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346151580287cbfded1d",
+    "url": "/nimble-code-challenge/static/js/main.180670eb.chunk.js"
+  },
+  {
+    "revision": "70f0f3f083189e651357",
+    "url": "/nimble-code-challenge/static/js/runtime-main.f002a3ad.js"
+  },
+  {
+    "revision": "15f048acc3e759f78bb7ac8289e53b97",
+    "url": "/nimble-code-challenge/static/media/down_caret.15f048ac.svg"
+  },
+  {
+    "revision": "8899ba3366419fb7fc88d0439c5305e2",
+    "url": "/nimble-code-challenge/static/media/icon-minus-with-circle.8899ba33.svg"
+  },
+  {
+    "revision": "05e01fa3c0c7a6396775397ae492f49a",
+    "url": "/nimble-code-challenge/static/media/icon-plus-with-circle.05e01fa3.svg"
+  },
+  {
+    "revision": "7f0971efc27e5b847bbd247cc5d57a1e",
+    "url": "/nimble-code-challenge/static/media/note.7f0971ef.svg"
+  },
+  {
+    "revision": "9f7596ff6328f5dade3a1d6949f2a3e2",
+    "url": "/nimble-code-challenge/static/media/right_caret.9f7596ff.svg"
+  },
+  {
+    "revision": "767765e6f2078bbb616f7f94d7e018da",
+    "url": "/nimble-code-challenge/static/media/scorecard.767765e6.svg"
+  },
+  {
+    "revision": "12b5a5f3e8a1eb84e8000874687ec78f",
+    "url": "/nimble-code-challenge/static/media/search_white.12b5a5f3.svg"
+  }
+]);
